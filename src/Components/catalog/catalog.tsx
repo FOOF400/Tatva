384c9b5880296f845928bad56ed33b0433f00a60
JosephKS10 <jjbrothersdelhi@hotmail.com>
2024-11-13 17:22:01 +0530

    Catalog page updated

--- src/Components/catalog/catalog.tsx
+++ src/Components/catalog/catalog.tsx
@@ -16,12 +16,16 @@ const Catalog = () => {
         <div className="info-card">
           <h2>About</h2>
           <p>
-            blah
+          Welcome to Tatva Essence, a pioneering company dedicated to transforming discarded produce into high-quality organic aged compost.
+
+          Based in Delhi NCR, we partner with local vendors and large suppliers to convert potential waste into valuable resources. By leveraging advanced technology and sustainable practices, we aim to create a greener future while fostering economic resilience in our communities. With a focus on innovation and collaboration, Tatva Essence is redefining waste management and setting new standards for sustainability in the industry.
+
+          At Tatva Essence, our journey began with a simple yet profound realization. Driven by a shared passion for sustainability, we discovered heaps of wasted produce in local mandis. This blatant disregard for resources ignited a spark within us, compelling us to take action.
           </p>
           <div className="contact-info">
-            <p>Website</p>
-            <p>Contact</p>
-            <p>Address</p>
+            <button className='contact-info-btn'>Website</button><br />
+            <button className='contact-info-btn'>Contact</button><br />
+            <button className='contact-info-btn'>Address</button>
           </div>
           <div className="social-icons">
             <img src={whatsapp} alt="WhatsApp" className="icon" />
